89ff93e457f2a3b8e8db66bf1c6add0fe83e8002
TaiLinh <linh.nguyenbuitai23@student.passerellesnumeriques.org>
2022-10-11 17:11:12 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
-const express = require('express')
-const morgan = require('morgan')
-const path = require('path')
-const  {engine} = require ('express-handlebars');
-const app = express()
-const port = 3000
-
-app.use(express.static(path.join(__dirname,'public')))
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const { engine } = require("express-handlebars");
+const app = express();
+const port = 3000;
+const routes = require("./routes");
+app.use(express.static(path.join(__dirname, "public")));
 //http loger
-app.use(morgan("combined"))
+// app.use(morgan("combined"))
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 //template engine
-app.engine('hbs', engine(
-  {
-    extname:"hbs"
-  }
-));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname,'resources/views'))
+app.engine(
+  "hbs",
+  engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
 
-app.get('/new', (req, res) => {
-  res.render('new');
-})
+// Route init
 
-app.get('/search', (req, res) => {
-  res.render('search');
-})
+routes(app);
 
-app.post('/search', (req, res) => {
-  res.render('search');
-})
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+// catch 404 and forward to error handler
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ src/routes/news.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const routes = express.Router()
+
+const newsController = require('../app/controllers/NewsController')
+module.exports = routes
